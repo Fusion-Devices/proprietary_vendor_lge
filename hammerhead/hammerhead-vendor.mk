2781801200acc09ff6f6132ee6980974c8ced0cd
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-09-04 19:44:57 +0200

    Massive update lge vendor.
    Credit lge devs.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- hammerhead/hammerhead-vendor.mk
+++ hammerhead/hammerhead-vendor.mk
@@ -31,7 +31,9 @@ DEVICE_PACKAGE_OVERLAYS := vendor/lge/hammerhead/overlay
 
 # Apps
 PRODUCT_PACKAGES += \
+    OmaDmclient \
     qcrilmsgtunnel \
+    SprintHiddenMenu \
     shutdownlistener \
     TimeService \
     UpdateSetting
